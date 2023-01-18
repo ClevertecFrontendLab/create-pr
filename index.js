ec5446c176c64596e814892a4e9334974d0b6d79
Igor Shidlovskiy <85307211+Gaurrus@users.noreply.github.com>
2023-01-18 20:04:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const main = async () => {
             repo,
             pull_number,
             reviewers: [
+                'Rastamanby',
                 'ValadzkoAliaksei',
                 'Gaurrus',
                 'dariavorom',
