f8cb0ec4b23b4173db9dd780c051f59086142760
mitrofazxc <mitrofanzxc@gmail.com>
2023-12-18 16:24:19 +0300

    feat(*): dynamic reviewers list

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
+
+const API_URL = 'https://training.clevertec.ru';
+const EXTRA_MENTORS_LIST = ['ValadzkoAliaksei'];
 
 const main = async () => {
   try {
@@ -7,25 +11,19 @@ const main = async () => {
     const repo = core.getInput('repo', { required: true });
     const pull_number = core.getInput('pull_number', { required: true });
     const token = core.getInput('token', { required: true });
+    const base_url = core.getInput('host', { required: false }) || API_URL;
+    const mentors_api_endpoint = `${base_url}/mentors`;
 
     const octokit = new github.getOctokit(token);
+
+    const response = await axios.get(mentors_api_endpoint);
+    const reviewers = response.data?.map((mentor) => mentor.github) ?? EXTRA_MENTORS_LIST;
 
     await octokit.rest.pulls.requestReviewers({
       owner,
       repo,
       pull_number,
-      reviewers: [
-        'Rastamanby',
-        'ValadzkoAliaksei',
-        'dariavorom',
-        'Ivan6813',
-        'graffad',
-        'vvikota',
-        'vsachenko',
-        'SergeyGlazun',
-        'Snoop593',
-        'Java-zhara',
-      ],
+      reviewers,
     });
   } catch (error) {
     core.setFailed(error.message);
